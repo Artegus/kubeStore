066eb7c76607134fc72c92e8ec286e5058bd82f6
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2021-02-19 19:55:17 +0100

    bye bye templates

--- kubeStore/public/carrito.php
+++ kubeStore/public/carrito.php
@@ -39,7 +39,7 @@
                         </a>
                         <a href="./carrito.php" class="btn btn-cart">
                             <i class="fas fa-shopping-cart"></i>
-                            <span class="cart-items">4</span>
+                            <!-- <span class="cart-items">0</span> -->
                         </a>
                         <a class="btn btn-outline-dark me-2 btn-action btn-logout">
                             Log out
@@ -60,86 +60,12 @@
 
                 <div class="container-fluid w-75">
                     <h5>Your cart</h5>
-
                     <!-- Start Cart items -->
-
-                        <div class="row">
-                            <div class="col-sm-9">
-                                <!-- start Cart item -->
-                                <div class="row border-top py-3 mt-3">
-                                    <div class="col-sm-2">
-                                        <img src="./images/shengshou-teraminx.jpg" alt="cart_item_1" class="img-fluid">
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <h6>Cube - XaSphi</h6>
-                                        <h6>Fabricante: Kinshotu</h6>
-                                        <!-- Amount -->
-                                        <div class="qty d-flex pt-2">
-                                            <div class="d-flex w-25">
-                                                <button class="qty-up border bg-light"><i class="fas fa-angle-up"></i></button>
-                                                <input type="text" class="qty_input border px-2 w-50 bg-light" disabled value="1">
-                                                <button class="qty-up border bg-light"><i class="fas fa-angle-down"></i></button>
-                                            </div>
-                                            <button type="submit" class="btn btn-outline-danger  px-3 m-auto">
-                                                Delete
-                                            </button>
-                                        </div>
-                                        <!-- Amount -->
-                                    </div>
-                                    <!-- start Price item-->
-                                    <div class="col-sm-2 text-right">
-                                        <div class="text-danger">
-                                            <span class="product_price">10</span> €
-                                        </div>
-                                    </div>
-                                    <!-- END Price item-->
-                                </div>
-                                <!-- end Cart item -->
-
-                                <div class="row border-top py-3 mt-3">
-                                    <div class="col-sm-2">
-                                        <img src="./images/shengshou-teraminx.jpg" alt="cart_item_1" class="img-fluid">
-                                    </div>
-                                    <div class="col-sm-8">
-                                        <h6>Cube - XaSphi</h6>
-                                        <h6>Fabricante: Kinshotu</h6>
-                                        <!-- Amount -->
-                                        <div class="qty d-flex pt-2">
-                                            <div class="d-flex w-25">
-                                                <button class="qty-up border bg-light"><i class="fas fa-angle-up"></i></button>
-                                                <input type="text" class="qty_input border px-2 w-50 bg-light" disabled value="1">
-                                                <button class="qty-up border bg-light"><i class="fas fa-angle-down"></i></button>
-                                            </div>
-                                            <button type="submit" class="btn btn-outline-danger px-3 m-auto">
-                                                Delete
-                                            </button>
-                                        </div>
-                                        <!-- Amount -->
-                                    </div>
-                                    <!-- start Price item-->
-                                    <div class="col-sm-2 text-right">
-                                        <div class="text-danger">
-                                            <span class="product_price">10</span> €
-                                        </div>
-                                    </div>
-                                    <!-- END Price item-->
-                                </div>
+                        <div class="row content-products">
+                            <div class="col-sm-9 content-products-cart">
+                                    
                             </div>
 
-                            <!-- START Subtotal section -->
-
-                            <div class="col-sm-3">
-                                <div class="sub-total border text-center mt-2">
-                                    <h6 class="text-success py-3">Your order is ready to buy</h6>
-                                    <div class="border-top py-4">
-                                        <h7 class="">Subtotal ( 2 items ) <span class="text-danger" id="total-price">20</span> €</h7>
-                                        
-                                    </div>
-                                    <button type="submit" class="btn btn-success">Buy</button>
-                                </div>
-                            </div>
-
-                            <!-- END Subtotal section -->
                         </div>
 
                     <!-- END Cart items -->
