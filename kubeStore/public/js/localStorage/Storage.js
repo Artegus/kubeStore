a396249c51fc8bd677c06b331420fc49a0c3afc8
Gabriel Agustín Uriarte <gabri02.uri@gmail.com>
2021-02-19 19:51:42 +0100

    add function to remove cart and defaultCart item

--- kubeStore/public/js/localStorage/Storage.js
+++ kubeStore/public/js/localStorage/Storage.js
@@ -10,6 +10,24 @@ class Storage {
         return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
     }
 
+    static removeCart() {
+        if (localStorage.getItem('cart')) {
+            localStorage.removeItem('cart')
+        }
+    }
+
+    static defaultCartItem (id, amount, product) {
+        return {
+            'product_id' : id,
+            'product_amount' : amount,
+            'product_price' : product.product_price,
+            'product_name' : product.product_name,
+            'product_brand' : product.product_brand,
+            'product_image' : product.product_image
+        }
+    }
+
+    
 }
 
 
